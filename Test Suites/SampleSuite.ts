5ca30ea3d5915e485d2828ddff43c2770119a51e
sakdas <sakdas@gmail.com>
2024-01-19 10:39:35 +0300

    sample suite eklendi
    sample suite eklendi

---
+++ Test Suites/SampleSuite.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>SampleSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>339b8362-38cb-4fa7-85f0-bb0c2350e29e</testSuiteGuid>
+   <testCaseLink>
+      <guid>432f6904-181e-4b4e-88be-ec80f4cfe40f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Regression/01_NewCustomer</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId></testDataLinkId>
+         <type>DEFAULT</type>
+         <value></value>
+         <variableId>539836a7-ce79-4e24-8e7a-c2c23894cb2e</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
